7a59e8a3e87f4d66e24c8123b6c113b054e050b8
Ewout <ewout@tallapplications.com>
2013-04-29 16:44:32 +0200

    Added flag.

---
+++ Dot42/TypeDefinition.Partial.cs
@@ -0,0 +1,13 @@
+﻿namespace Mono.Cecil
+{
+    /// <summary>
+    /// Add extensions
+    /// </summary>
+    partial class TypeDefinition
+    {
+        /// <summary>
+        /// Is this type used in a Nullable(T) construct?
+        /// </summary>
+        public bool UsedInNullableT { get; set; }
+    }
+}
