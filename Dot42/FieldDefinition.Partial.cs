4c2a6cecd699cdceaf45667e009d0f6fc7b1f016
Olaf Pettersson <olaf.pettersson@web.de>
2015-04-21 18:57:30 +0200

    System.Threading.Interlocked implementation based on AtomicXXXFieldUpdater

---
+++ Dot42/FieldDefinition.Partial.cs
@@ -0,0 +1,13 @@
+﻿namespace Mono.Cecil
+{
+    /// <summary>
+    /// Add extensions
+    /// </summary>
+    partial class FieldDefinition
+    {
+        /// <summary>
+        /// Is this type used in an Interlocked.XXX call as reference?
+        /// </summary>
+        public bool IsUsedInInterlocked { get; set; }
+    }
+}
