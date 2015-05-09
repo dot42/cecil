bba005952111922d1dfb45cc7a38e89d51c02aaa
Olaf Pettersson <olaf.pettersson@web.de>
2015-05-09 13:00:11 +0200

    retain original name for renamed methods

---
+++ Dot42/MethodDefinition.Partial.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+
+namespace Mono.Cecil
+{
+    public partial class MethodDefinition
+    {
+        private string originalName;
+        public string OriginalName { get { return originalName ?? Name; } }
+
+        /// <summary>
+        /// use this method to set a new name, but preserve the original name
+        /// in OriginalName
+        /// </summary>
+        public void SetName(string name)
+        {
+            Interlocked.CompareExchange(ref originalName, Name, null);
+            Name = name;
+        }
+    }
+}
